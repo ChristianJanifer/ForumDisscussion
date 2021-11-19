f9efb1ab86b1cdac90c7b12b409daad48daaaf4d
cindyrizki <cindyamalya428@gmail.com>
2021-11-19 11:42:00 +0700

    Register

--- API-Forum/API-Forum/Controllers/UsersController.cs
+++ API-Forum/API-Forum/Controllers/UsersController.cs
@@ -1,17 +1,41 @@
 ﻿using API_Forum.Controllers.Base;
 using API_Forum.Models;
 using API_Forum.Repository.Data;
+using API_Forum.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace API_Forum.Controllers
 {
     public class UsersController : BaseController<User, UserRepository, int>
     {
+        private readonly UserRepository user;
         public UsersController(UserRepository userRepository) : base(userRepository)
         {
+            this.user = userRepository;
+        }
+
+        [Route("Register")]
+        [HttpPost]
+        public ActionResult Register(RegisterVM registerVM)
+        {
+            var result = user.Register(registerVM);
+            if (result == 2)
+            {
+                return BadRequest(new { status = HttpStatusCode.BadRequest, message = "Email sudah terdaftar" });
+            }
+            else if (result == 3)
+            {
+                return BadRequest(new { status = HttpStatusCode.BadRequest, message = "Nomor telepon sudah terdaftar" });
+            }
+            else
+            {
+                return Ok(new { HttpStatusCode.OK });
+            }
 
         }
     }
--- API-Forum/API-Forum/Repository/Data/UserRepository.cs
+++ API-Forum/API-Forum/Repository/Data/UserRepository.cs
@@ -1,5 +1,7 @@
 ﻿using API_Forum.Context;
+using API_Forum.HashingPassword;
 using API_Forum.Models;
+using API_Forum.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,65 @@ namespace API_Forum.Repository.Data
 {
     public class UserRepository : GeneralRepository<MyContext, User, int>
     {
+        private readonly MyContext context;
         public UserRepository(MyContext myContext) : base(myContext)
         {
-
+            this.context = myContext;
         }
+
+		public int Register(RegisterVM registerVM)
+		{
+			var checkEmail = context.Users.Where(p => p.Email == registerVM.Email).FirstOrDefault();
+			var checkPhone = context.Users.Where(p => p.Phone == registerVM.Phone).FirstOrDefault();
+			if (checkEmail != null)
+			{
+				return 2;
+			}
+			else if (checkPhone != null)
+			{
+				return 3;
+			}
+			else
+			{
+				var userResult = new User
+				{
+					FirstName = registerVM.FirstName,
+					LastName = registerVM.LastName,
+					Gender = (Models.Gender)registerVM.Gender,
+					BirthDate = registerVM.BirthDate,
+					Phone = registerVM.Phone,
+					Email = registerVM.Email,
+					Account = new Account
+					{
+						Password = Hashing.HashPassword(registerVM.Password)
+					}
+				};
+				context.Users.Add(userResult);
+				context.SaveChanges();
+
+				int checkRole = registerVM.RoleId;
+				int roleId = 0;
+
+				if(checkRole == 1)
+                {
+					roleId += 1;
+				}
+				else if(checkRole == 2)
+                {
+					roleId += 2;
+				}
+
+
+				var userRole = new AccountRole
+				{
+					UserId = userResult.UserId,
+					RoleId = roleId
+				};
+
+				context.AccountRoles.Add(userRole);
+				var result = context.SaveChanges();
+				return result;
+			}
+		}
     }
 }
